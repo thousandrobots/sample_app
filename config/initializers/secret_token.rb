ac2ff71a2acc86fbf5afb41ed4361b70013e20ac
Thousand Robots <a@obscureobjects>
2012-07-10 20:34:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2c47bce43a7a667474fd0f4817f447baa5f6758622146552bb86613b50d5150e63abb45ea38c3e1545d9c655fd6dd1d93cddba1d8147e8f479ebdeb788fab82d'
